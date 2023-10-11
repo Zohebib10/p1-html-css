38f98059de4771b914e23d496c150b2ac7803bce
Zohebib10 <9023992@student.zadkine.nl>
2023-10-11 09:14:13 +0200

    6.3

---
+++ 6.3 javascript/6.3.js
@@ -0,0 +1,29 @@
+function MijnFunctie()
+{
+document.getElementById('mijnHeader').style.color = "blue";
+document.getElementById('mijnHeader').innerHTML = 'IK BEN ER!'
+
+}
+
+
+function calculate(getal1, getal2)
+{
+antwoord = getal1 + getal2;
+console.log("Hello world!");
+console.log (antwoord);
+
+}
+
+
+calculate(5,8);
+
+
+
+function Reken(getal1, getal2)
+{
+antwoord = getal1 * getal2;
+console.log("Hello world!");
+console.log (antwoord);
+
+}
+
