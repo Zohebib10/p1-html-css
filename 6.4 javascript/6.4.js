7eb0a483ed45cdfb40b46851652fb2f103f45e1a
Zohebib10 <9023992@student.zadkine.nl>
2023-10-11 09:29:21 +0200

    6.2

---
+++ 6.4 javascript/6.4.js
@@ -0,0 +1,34 @@
+function calculate(getal1, getal2)
+{
+antwoord = getal1 + getal2;
+console.log("Hello world!");
+console.log (antwoord);
+
+}
+
+
+calculate(5,8);
+
+
+
+function Reken(getal1, getal2)
+{
+antwoord = getal1 * getal2;
+console.log("Hello world!");
+console.log (antwoord);
+
+}
+
+
+Reken(7,5);
+
+function calculateMinutes(getal1, getal2)
+{
+antwoord = getal1 / getal2;
+console.log("Hello world!");
+console.log (antwoord);
+
+}
+
+
+calculateMinutes(3600,60);
