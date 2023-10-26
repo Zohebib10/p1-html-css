6bfd0f2104976ac0125fdd8852e8baf2031eae3e
Zohebib10 <9023992@student.zadkine.nl>
2023-10-26 15:11:17 +0200

    7.3

---
+++ 7.1 javascript/java.js
@@ -0,0 +1,13 @@
+var x;
+if (100)
+{
+// executed only if "condition" is true
+}
+else if (60)
+{
+// executed only if "condition" was false and "other condition" is true
+}
+else
+{
+// executed only if both "condition" and "other condition" were false
+}
